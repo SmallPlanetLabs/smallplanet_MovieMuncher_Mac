64e0a9b00e3966ee40a9a6a87974cb8d551dea78
KittyMac <rocco@chimerasw.com>
2014-08-20 16:18:28 -0400

    Initial Commit

---
+++ MovieMuncher/MMDocument.h
@@ -0,0 +1,13 @@
+//
+//  MMDocument.h
+//  MovieMuncher
+//
+//  Created by Rocco Bowling on 8/20/14.
+//  Copyright (c) 2014 Rocco Bowling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MMDocument : NSDocument
+
+@end
---
+++ MovieMuncher/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MovieMuncher
+//
+//  Created by Rocco Bowling on 8/20/14.
+//  Copyright (c) 2014 Rocco Bowling. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
